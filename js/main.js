8b8e4fc114bdf0392d737c616db23565e36a795d
Yuriy Zhmurko <777zhm@gmail.com>
2019-01-15 11:15:26 +0200

    sidebar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 (function($) {
 $( document ).ready(function() {
 
+// sidebar
+$('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
 
+
+// stars rating
 $('.star.rating').click(function(){
   console.log( $(this).parent().data('stars') + ", " + $(this).data('rating'));
   $(this).parent().attr('data-stars', $(this).data('rating'));
