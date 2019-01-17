dd7243b027c2250f8943896a8b15d7cdf6858919
Yuriy Zhmurko <777zhm@gmail.com>
2019-01-17 16:15:13 +0200

    tabs navigation

--- js/main.js
+++ js/main.js
@@ -13,11 +13,23 @@
 			$(this).parent().attr('data-stars', $(this).data('rating'));
 		});
 
+		
+		
 		// Bootstrap tabs on account page
 		$('#myTab a').on('click', function (e) {
 		e.preventDefault()
 		$(this).tab('show')
 		});
+
+		// Bootstrap tabs anchors
+		if( $('#nav-tab').length ){
+			var hash = window.location.hash.substr(1);
+			if( hash ){
+				$('.nav-tabs a[href="#' + hash + '"]').tab('show');
+			}
+		}
+
+
 
 		//Account page add new address show div
 		$('#addNewAddressButton').click(function() {
@@ -26,4 +38,6 @@
 		});
 
 	});
+
+
 })(jQuery);
